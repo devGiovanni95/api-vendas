dd9c9c2455b3686ace0acdd65c757513638f0bd6
devGiovanni95 <giovanni.devjava@hotmail.com>
2023-07-16 15:43:33 -0300

    Created role business of duplicity name

---
+++ src/modules/products/typeorm/services/CreatedProductService.ts
@@ -0,0 +1,34 @@
+import { getCustomRepository } from 'typeorm';
+import { ProductRepository } from '../repositories/ProductRepository';
+import AppError from '@shared/errors/AppError';
+import { Product } from '../entities/Product';
+
+interface IRequest {
+  name: string;
+  price: number;
+  quantity: number;
+}
+
+class CreateProductService {
+  public async execute({ name, price, quantity }: IRequest): Promise<Product> {
+    const productsRepository = getCustomRepository(ProductRepository);
+    /**
+     * Confere se existe um produto com mesmo nome
+     */
+    const productExists = await productsRepository.findByName(name);
+
+    if (productExists) {
+      throw new AppError('There is alread one product eith this name');
+    }
+
+    const product = productsRepository.create({
+      name,
+      price,
+      quantity,
+    });
+
+    await productsRepository.save(product);
+
+    return product;
+  }
+}
