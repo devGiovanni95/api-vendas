b5d7c82c85c3aa63df23695209e92e57b91689a2
devGiovanni95 <giovanni.devjava@hotmail.com>
2023-07-16 19:18:10 -0300

    Created service for update products

---
+++ src/modules/products/typeorm/services/UpdateProductService.ts
@@ -0,0 +1,44 @@
+import { UpdateDateColumn, getCustomRepository } from 'typeorm';
+import { ProductRepository } from '../repositories/ProductRepository';
+import AppError from '@shared/errors/AppError';
+import { Product } from '../entities/Product';
+
+interface IRequest {
+  id: string;
+  name: string;
+  price: number;
+  quantity: number;
+}
+class UpdateProductService {
+  public async execute({
+    id,
+    name,
+    price,
+    quantity,
+  }: IRequest): Promise<Product> {
+    const productsRepository = getCustomRepository(ProductRepository);
+
+    const product = await productsRepository.findOne(id);
+
+    if (!product) {
+      throw new AppError('Product not found.');
+    }
+    /**
+     * Confere se o novo nome ja existe em outro produto
+     */
+    const productExists = await productsRepository.findByName(name);
+
+    if (productExists && name !== product.name) {
+      throw new AppError('There is alread one product eith this name');
+    }
+
+    product.name = name;
+    product.price = price;
+    product.quantity = quantity;
+
+    await productsRepository.save(product);
+
+    return product;
+  }
+}
+export default UpdateProductService;
