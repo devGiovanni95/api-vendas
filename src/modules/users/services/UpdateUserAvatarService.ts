ff8618f3c59f68853083c90c053fa9f4dd1b75be
devGiovanni95 <giovanni.devjava@hotmail.com>
2023-07-18 20:32:28 -0300

    Created service for update of rhe avatar image

---
+++ src/modules/users/services/UpdateUserAvatarService.ts
@@ -0,0 +1,44 @@
+import { getCustomRepository } from 'typeorm';
+import AppError from '@shared/errors/AppError';
+import User from '../typeorm/entities/User';
+import UsersRepository from '../typeorm/repositories/UsersRepository';
+import path from 'path';
+import uploadConfig from '@config/upload';
+import fs from 'fs';
+
+interface IRequest {
+  user_id: string;
+  avatarFilename: string;
+}
+
+class UpdateUserAvatarService {
+  public async execute({ user_id, avatarFilename }: IRequest): Promise<User> {
+    const usersRepository = getCustomRepository(UsersRepository);
+
+    const user = await usersRepository.findById(user_id);
+
+    if (!user) {
+      throw new AppError('User not found.');
+    }
+
+    if (user.avatar) {
+      //pegando o nome do arquivo
+      const userAvatarFilePath = path.join(uploadConfig.directory, user.avatar);
+      //verifica se existe o arquivo
+      const userAvatarFileExists = await fs.promises.stat(userAvatarFilePath);
+
+      if (userAvatarFileExists) {
+        //se existir apagamos o arquivo
+        await fs.promises.unlink(userAvatarFilePath);
+      }
+    }
+
+    user.avatar = avatarFilename;
+
+    await usersRepository.save(user);
+
+    return user;
+  }
+}
+
+export default UpdateUserAvatarService;
