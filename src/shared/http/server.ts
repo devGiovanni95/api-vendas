cabab9950d9f5add3384d39575e26beadfa58605
devGiovanni95 <giovanni.devjava@hotmail.com>
2023-07-18 21:06:21 -0300

    Created static route for to facilitate request of the frontEnd

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -7,11 +7,13 @@ import routes from './routes';
 import { request } from 'http';
 import AppError from '@shared/errors/AppError';
 import '@shared/typeorm';
+import uploadConfig from '@config/upload';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use('/files', express.static(uploadConfig.directory));
 
 app.use(routes);
 
