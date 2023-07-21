ac73aa989f6f123a61c536229615220f334e6685
devGiovanni95 <giovanni.devjava@hotmail.com>
2023-07-20 21:06:44 -0300

    Call file for body of the email

--- src/modules/users/services/SendForgotPasswordEmailService.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -2,6 +2,7 @@ import { getCustomRepository } from 'typeorm';
 import AppError from '@shared/errors/AppError';
 import UsersRepository from '../typeorm/repositories/UsersRepository';
 import { hash } from 'bcryptjs';
+import path from 'path';
 import UserTokensRepository from '../typeorm/repositories/UserTokensRepository';
 import EtherealMail from '@config/mail/EtherealMail';
 interface IRequest {
@@ -25,6 +26,13 @@ class SendForgotPasswordEmailService {
       token,
     );
 
+    const forgotPasswordTemplate = path.resolve(
+      __dirname,
+      '..',
+      'views',
+      'forgot_password.hbs',
+    );
+
     await EtherealMail.sendMail({
       to: {
         name: user.name,
@@ -32,10 +40,10 @@ class SendForgotPasswordEmailService {
       },
       subject: '[API VENDAS] Recuperação de senha',
       templateData: {
-        template: `Olá {{name}}: {{token}}`,
+        file: forgotPasswordTemplate,
         variables: {
           name: user.name,
-          token,
+          link: `http://localhost:3000/reset_password?token=${token}`,
         },
       },
     });
