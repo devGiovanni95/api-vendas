19dbc046ec118f32d62db0835e3eada9b1fa4ac1
devGiovanni95 <giovanni.devjava@hotmail.com>
2023-07-21 19:13:59 -0300

    Created route for show and update profiles

---
+++ src/modules/users/routes/profile.routes.ts
@@ -0,0 +1,44 @@
+import { Router } from 'express';
+import { celebrate, Joi, Segments } from 'celebrate';
+import ProfileController from '../controllers/ProfileController';
+import isAuthenticated from '@shared/http/middlewares/isAuthenticated';
+
+const profileRouter = Router();
+const profileController = new ProfileController();
+
+profileRouter.use(isAuthenticated);
+
+profileRouter.get('/', profileController.show);
+
+profileRouter.put(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      email: Joi.string().email().required(),
+      old_password: Joi.string(),
+      password: Joi.string().optional(),
+      password_confirmation: Joi.string()
+        .valid(Joi.ref('password'))
+        .when('password', {
+          is: Joi.exist(),
+          then: Joi.required(),
+        }),
+    },
+  }),
+  profileController.update,
+);
+/*
+profileRouter.post(
+  '/reset',
+  celebrate({
+    [Segments.BODY]: {
+      token: Joi.string().uuid().required(),
+      password: Joi.string().required(),
+      password_confirmation: Joi.string().required().valid(Joi.ref('password')),
+    },
+  }),
+  profileController
+);
+*/
+export default profileRouter;
