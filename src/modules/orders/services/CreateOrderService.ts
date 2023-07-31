b85fbf501cb33b013bcd4e78f34a9168e9ddee53
devGiovanni95 <giovanni.devjava@hotmail.com>
2023-07-30 22:14:47 -0300

    Created comparation between quantity in stock and in the order

--- src/modules/orders/services/CreateOrderService.ts
+++ src/modules/orders/services/CreateOrderService.ts
@@ -21,28 +21,46 @@ class CreateOrderService {
     const customersRepository = getCustomRepository(CustomersRepository);
     const productsRepository = getCustomRepository(ProductRepository);
 
-
+    //verifica se ha um customer
     const customerExists = await customersRepository.findById(customer_id);
 
     if (!customerExists) {
       throw new AppError('Could not find any custumer with the given id.');
     }
 
+    //verifica se ha produtos
     const existsProduct = await productsRepository.findByIds(products);
 
     if(!existsProduct.length){
-        throw new AppError('Could not find any products with the given ids.')
+        throw new AppError('Could not find any products with the given ids.');
     }
     
+    //verifica se todos os produtos existem no banco de dados
     const existsProductsIds = existsProduct.map((product) => product.id);
     
     const checkInexistentProducts = products.filter(
       product => !existsProductsIds.includes(product.id),
-      )
+      );
       
       if(checkInexistentProducts.length){
-          throw new AppError(`Could not find product ${checkInexistentProducts[0].id}.`)
+          throw new AppError(`Could not find product ${checkInexistentProducts[0].id}.`);
       }
+
+      //verificando se quantidade é menor que a disponivel do sistema
+      const quantityAvailable = products.filter(
+        product => existsProduct.filter(
+          p => p.id === product.id
+        )[0].quantity<product.quantity,
+      )
+
+      //se for menor gera uma excessão
+      if(quantityAvailable.length){
+        throw new AppError(
+          `The quantity ${quantityAvailable[0].quantity} 
+          is not available for ${quantityAvailable[0].id}.`);
+    }
+    
+
 
   }
 }
